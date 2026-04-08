8df321771f42922f20efe91f1645a413239f83c0
laurexsol <laurinha.demelosol@gmail.com>
2026-04-08 13:10:16 -0300

    Add files via upload

---
+++ energia.py
@@ -0,0 +1,6 @@
+potencia = float(input("digite a potência: "))
+tempo = float(input("digite o tempo: "))
+
+energia = potencia * tempo 
+
+print("Energia ", energia)
---
+++ precoporpessoa.py
@@ -0,0 +1,5 @@
+total = float(input("digite o valoe total: "))
+pessoas = float(input("quants pessoas: "))
+
+valor = total/pessoas 
+print("cda pessoa paga", valor)
