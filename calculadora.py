42d1f8c4347b3fc7175b0f1d20516e19b847fcfa
laurexsol <laurinha.demelosol@gmail.com>
2026-08-03 13:09:32 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+soma = n1 + n2
+multiplicacao = n1 * n2
+subtracao = n1 - n2
+divisao = n1 / n2
+
+print("A soma de ", n1, "e", n2, "é", soma)
+print("A multiplicacao de", n1, "e", n2, "é", multiplicacao)
+print("A subtracao de", n1, "e", n2, "é", subtracao)
+print("A divisao de", n1, "e", n2, "é", divisao)
