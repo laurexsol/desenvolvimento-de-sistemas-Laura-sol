dc5941188cc0dac1489e15ef778d6b321869964f
laurexsol <laurinha.demelosol@gmail.com>
2026-08-19 12:35:28 -0300

    Add files via upload

---
+++ distanciade.2pontos.py
@@ -0,0 +1,8 @@
+x1 = float(input("Digite o x1: "))
+x2 = float(input("Digite o x2: "))
+y1 = float(input("Digite o y1: "))
+y2 = float(input("Digite o y2: "))
+
+distancia = ((x2 - x1)**2 + (y2 - y1)**2)**0,5
+
+print("A distancia entre os pontos é: ", distancia)
